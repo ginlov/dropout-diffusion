78a9e8b53361727a734d532d14ee01c6683c5570
Vũ Long Giang <longgiang2015bn@gmail.com>
2022-11-13 17:01:01 +0700

    init: start project

---
+++ dropout_diffusion/__init__.py
@@ -0,0 +1,7 @@
+"""
+Codebase for "Improved Denoising Diffusion Probabilistic Models".
+"""
+
+"""
+Modified codebase of Improved Denoising Diffusion Probabilistic Models called Dropout Diffusion
+"""
